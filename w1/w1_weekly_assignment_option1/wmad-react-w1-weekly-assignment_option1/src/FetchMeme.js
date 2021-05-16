fe24ca51d510c43cb6a9dc9aa69c91b17ce710d4
miyabitanimchi <69213541+miyabitanimchi@users.noreply.github.com>
2021-05-15 18:23:11 -0700

    add functions

--- w1/w1_weekly_assignment_option1/wmad-react-w1-weekly-assignment_option1/src/FetchMeme.js
+++ w1/w1_weekly_assignment_option1/wmad-react-w1-weekly-assignment_option1/src/FetchMeme.js
@@ -1,22 +1,64 @@
 import React from "react";
+import Meme from "./Meme";
 import "./FetchMeme.css";
 
 class FetchMeme extends React.Component {
   state = {
     loading: true,
+    imgSrc: "",
+    imgName: "",
+    boxCount: 0,
   };
 
   async componentDidMount() {
-    const url = "https://api.imgflip.com/get_memes";
-    const response = await fetch(url);
-    const data = await response.json();
-    console.log(data);
+    try {
+      const response = await fetch("https://api.imgflip.com/get_memes");
+      const data = await response.json();
+      console.log(data);
+      this.setState({
+        loading: false,
+        imgSrc: data.data.memes[0].url,
+        imgName: data.data.memes[0].name,
+        boxCount: data.data.memes[0].box_count,
+      });
+    } catch (error) {
+      console.log(`Oops! error: ${error}`);
+    }
   }
+
+  getRandomImg = async () => {
+    try {
+      const response = await fetch("https://api.imgflip.com/get_memes");
+      const data = await response.json();
+      const randomIndex = Math.floor(Math.random() * 100);
+      console.log(data.data.memes[randomIndex].box_count);
+
+      this.setState({
+        loading: false,
+        imgSrc: data.data.memes[randomIndex].url,
+        imgName: data.data.memes[randomIndex].name,
+        boxCount: data.data.memes[randomIndex].box_count,
+      });
+    } catch (error) {
+      console.log(`Oops! error: ${error}`);
+    }
+  };
 
   render() {
     return (
       <div>
-        {this.state.loading ? <div>Loading...</div> : <div>meme...</div>}
+        {this.state.loading ? (
+          <div>Loading...</div>
+        ) : (
+          <Meme
+            src={this.state.imgSrc}
+            name={this.state.imgName}
+            boxCount={this.state.boxCount}
+          />
+        )}
+
+        <button onClick={this.getRandomImg}>Get image</button>
+        <button>Create meme with this image</button>
       </div>
     );
   }
--- w1/w1_weekly_assignment_option1/wmad-react-w1-weekly-assignment_option1/src/Meme.js
+++ w1/w1_weekly_assignment_option1/wmad-react-w1-weekly-assignment_option1/src/Meme.js
@@ -1,3 +1,22 @@
 import React from "react";
-import FetchAPI from "FetchAPI";
+// import FetchMeme from "./FetchMeme";
 import "./Meme.css";
+
+const Meme = (props) => {
+  const createInputs = () => {
+    let inputs = [];
+    for (let index = 1; index <= props.boxCount; index++) {
+      inputs.push(<input type="text" name="text" key={index} />);
+    }
+    console.log(props.boxCount);
+    console.log(inputs);
+  };
+  return (
+    <div>
+      <img src={props.src} alt={props.name} />
+      <form>{createInputs()}</form>
+    </div>
+  );
+};
+
+export default Meme;
